b804d69d5ed14fd1a3560a539a546aaa4f618b6b
Marek Barszcz <marek.barszcz@fortedigital.no>
2022-09-27 12:54:16 +0200

    Add extension method for .NET6 DI, add constants files

---
+++ Forte.EpiImageHotSpots/Forte.EpiImageHotSpots/Configuration/Constants.cs
@@ -0,0 +1,6 @@
+﻿namespace Forte.EpiImageHotSpots.Configuration;
+
+internal static class Constants
+{
+    internal const string ModuleName = "Forte.EpiImageHotSpots";
+}
---
+++ Forte.EpiImageHotSpots/Forte.EpiImageHotSpots/ServiceCollectionExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using EPiServer.Shell.Modules;
+using Forte.EpiImageHotSpots.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Forte.EpiImageHotSpots;
+
+// ReSharper disable once UnusedType.Global
+public static class ServiceCollectionExtensions
+{
+    
+    // ReSharper disable once UnusedMember.Global
+    public static IServiceCollection AddForteEpiResponsivePicture(this IServiceCollection services) 
+        => services.Configure<ProtectedModuleOptions>(options =>
+        {
+            if (!options.Items.Any(x => x.Name.Equals(Constants.ModuleName)))
+            {
+                options.Items.Add(
+                    new ModuleDetails
+                    {
+                        Name = Constants.ModuleName,
+                    }
+                );
+            }
+        });
+}
